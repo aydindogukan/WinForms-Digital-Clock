9e0d382ae1c462a0515ffd44ed731839f3e43ffc
Dogukan Aydin <49591849+aydindogukan@users.noreply.github.com>
2022-10-25 08:00:36 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WinForms_Digital_Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblSecond = new System.Windows.Forms.Label();
+            this.lblDay = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.lblTime2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            resources.ApplyResources(this.lblTime, "lblTime");
+            this.lblTime.ForeColor = System.Drawing.Color.Lime;
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Click += new System.EventHandler(this.lblTime_Click);
+            // 
+            // lblSecond
+            // 
+            resources.ApplyResources(this.lblSecond, "lblSecond");
+            this.lblSecond.ForeColor = System.Drawing.Color.Lime;
+            this.lblSecond.Name = "lblSecond";
+            // 
+            // lblDay
+            // 
+            resources.ApplyResources(this.lblDay, "lblDay");
+            this.lblDay.ForeColor = System.Drawing.Color.Lime;
+            this.lblDay.Name = "lblDay";
+            // 
+            // lblDate
+            // 
+            resources.ApplyResources(this.lblDate, "lblDate");
+            this.lblDate.ForeColor = System.Drawing.Color.Lime;
+            this.lblDate.Name = "lblDate";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // lblTime2
+            // 
+            resources.ApplyResources(this.lblTime2, "lblTime2");
+            this.lblTime2.ForeColor = System.Drawing.Color.Lime;
+            this.lblTime2.Name = "lblTime2";
+            this.lblTime2.Click += new System.EventHandler(this.lblTime2_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.Controls.Add(this.lblTime2);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblDay);
+            this.Controls.Add(this.lblSecond);
+            this.Controls.Add(this.lblTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblTime;
+        private Label lblSecond;
+        private Label lblDay;
+        private Label lblDate;
+        private System.Windows.Forms.Timer timer;
+        private Label lblTime2;
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# WinForms-Digital-Clock
+A digital clock app developed in Windows Forms.
+
+![screenshot](./img/screenshot.png)
+
+## Installation
+- Clone the source code:
+
+```
+git clone https://github.com/aydindogukan/WinForms-Digital-Clock
+```
+
+- Open the `WinForms-Digital-Clock.sln` file in Visual Studio.
+- Run the source code.
